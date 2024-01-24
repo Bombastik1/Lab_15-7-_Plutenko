bbc09bc0bbb320ad9b56428825a0c781d2be8e6b
Bombastik1 <plutenko.ay@gmail.com>
2024-01-24 20:55:08 +0200

    Добавьте файлы проекта.

---
+++ Lab_15(7)_Plutenko/Lab_15(7)_Plutenko.cpp
@@ -0,0 +1,33 @@
+﻿#include <stdio.h> 
+#include <limits.h> 
+#include <locale>
+
+int main() {
+    setlocale(LC_ALL, "");
+    int n;
+    int i;
+
+    printf("Введiть кiлькiсть елементiв масиву: ");
+    scanf_s("%d", &n);
+    int* array = (int*)malloc(n * sizeof(int));
+    printf("Введiть елементи масиву:\n");
+    for (i = 0; i < n; i++) {
+        scanf_s("%d", &array[i]);
+    }
+
+    int s = 0;
+    for (int i = 0; i < n; i++) {
+        s += array[i];
+    }
+    printf("Загальна сума елементiв масиву: %d\n", s);
+
+    if (n < 10) {
+        int m = 1;
+        for (int i = 0; i < n; i++) {
+            m *= array[i];
+        }
+        printf("Загальний добуток елементiв масиву: %d\n", m);
+    }
+
+    return 0;
+}
